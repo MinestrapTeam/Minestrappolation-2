bcf301b29b3e3f0d92083cc3fb8f3ee22f36aa69
Domenic Portuesi <sobiohazardous@gmail.com>
2013-08-25 20:25:16 -0400

    Added Armor (not finished)

---
+++ sobiohazardous/minestrappolation/api/item/MItemArmor.java
@@ -0,0 +1,13 @@
+package sobiohazardous.minestrappolation.api.item;
+
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.ItemArmor;
+
+public class MItemArmor extends ItemArmor
+{
+	public MItemArmor(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4) 
+	{
+		super(par1, par2EnumArmorMaterial, par3, par4);
+	}
+
+}
