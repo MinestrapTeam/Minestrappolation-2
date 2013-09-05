4f6e6590cf884de4199e35d943022b927620ed2e
Dylan Greer <greerd240@yahoo.com>
2013-09-04 20:54:02 -0400

    Revert "Changed custom melter recipe file"
    This reverts commit 11a1d93ce5a02d7842b4df01d74238ba16881b2c.

--- sobiohazardous/minestrappolation/extraores/lib/ModdedMelterRecipeLoader.java
+++ sobiohazardous/minestrappolation/extraores/lib/ModdedMelterRecipeLoader.java
@@ -1,62 +1,62 @@
 package sobiohazardous.minestrappolation.extraores.lib;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 
-public class ModdedMelterRecipeLoader {
+import net.minecraft.client.Minecraft;
+
+public class ModdedMelterRecipeLoader 
+{
 	public static int[] ids = new int[2000];
 	public static int[] itemids = new int[2000];
 	
-	public  void loadModdedMelter(){
+	public  void loadModdedMelter()
+	{
 		File file = new File("config/MelterCanMelt.txt");
 		String line;
 		String num = "";
 		String num2 = "";
 		int arrayID = 0;
 		int iarrayID = 0;
-		try {
-			 FileWriter fw = new FileWriter(file,false);
-			 BufferedWriter bw = new BufferedWriter(fw);
-			 FileInputStream stream = new FileInputStream(file);
-			 DataInputStream in = new DataInputStream(stream);
-			 BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			if(!file.exists()){
+		try 
+		{
+			if(!file.exists())
+			{
 				file.createNewFile();
-				
 			}
-			bw.write("# Add melter recipes here recipe Ex. blockTOMeltID,ItemToMeltToID Ex. 14,353");
-			bw.close();
-		 
-		 
-		while((line = br.readLine()) !=null){
-			if(line.length() == 0){
-				continue;
-			 }
-			if(line.startsWith("#")){
-				continue;
-			}else
-			num = line.substring(0, line.indexOf(','));
-			num2 = line.substring(line.indexOf(',')+1);
-			ids[arrayID] = Integer.parseInt(num);
-			itemids[iarrayID] = Integer.parseInt(num2);
-		arrayID++;
-		iarrayID++;
-		}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		
+			FileInputStream stream = new FileInputStream(file);
+		 	DataInputStream in = new DataInputStream(stream);
+		 	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		
+		 	while((line = br.readLine()) !=null)
+		 	{
+			 	num = line.substring(0, line.indexOf(','));
+			 	num2 = line.substring(line.indexOf(',')+1);
+			 	ids[arrayID] = Integer.parseInt(num);
+			 	itemids[iarrayID] = Integer.parseInt(num2);
+			 	arrayID++;
+			 	iarrayID++;
+		 	}
+		} 
+		
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
 		}
-		System.out.println("Successfuly loaded custom melter recipes");
+		
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		System.out.println("Minestrappolation: Successfuly loaded custom melter recipes");
 		System.out.println(file.getAbsolutePath());
 	}
 
