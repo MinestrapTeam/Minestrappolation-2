799d5e2dd80403997aaa68a5dcaf8a4f03bb367e
Domenic Portuesi <sobiohazardous@gmail.com>
2013-10-07 15:45:36 -0400

    Changed Version and Info to local github

--- sobiohazardous/minestrappolation/api/ConnectionHandler.java
+++ sobiohazardous/minestrappolation/api/ConnectionHandler.java
@@ -11,7 +11,7 @@ import cpw.mods.fml.common.network.Player;
 
 public class ConnectionHandler implements IConnectionHandler {
 
-	String url = "https://raw.github.com/werty1124/VersionChecker/master/version.txt";
+	String url = "https://raw.github.com/sobiohazardous/minestrappolation-recode/info.text";
 	@Override
 	public void playerLoggedIn(Player player, NetHandler netHandler,
 			INetworkManager manager) {
