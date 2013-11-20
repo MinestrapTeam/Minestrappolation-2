06c291a3bfb799d3c579e7e2cb73eecfe6b193e0
Domenic Portuesi <sobiohazardous@gmail.com>
2013-11-19 21:12:59 -0500

    Bug Finalizing Again
    -Removed mossy planks mechanic
    -Made mossy planks spawn in abandon mineshafts

--- sobiohazardous/minestrappolation/extradecor/lib/EDTileEntityManager.java
+++ sobiohazardous/minestrappolation/extradecor/lib/EDTileEntityManager.java
@@ -6,7 +6,6 @@ import sobiohazardous.minestrappolation.extradecor.tileentity.TileEntityCrate;
 import sobiohazardous.minestrappolation.extradecor.tileentity.TileEntityEdgeStone;
 import sobiohazardous.minestrappolation.extradecor.tileentity.TileEntityEdgeStoneCorner;
 import sobiohazardous.minestrappolation.extradecor.tileentity.TileEntityGoblet;
-import sobiohazardous.minestrappolation.extradecor.tileentity.TileEntityPlanks;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class EDTileEntityManager {
@@ -15,7 +14,6 @@ public class EDTileEntityManager {
 		GameRegistry.registerTileEntity(TileEntityCrate.class, "tileEntityCrate");
 		GameRegistry.registerTileEntity(TileEntityBarrel.class, "tileEntityBarrel");
 		GameRegistry.registerTileEntity(TileEntityCardboardWet.class, "tileEntityCardboardWet");
-		GameRegistry.registerTileEntity(TileEntityPlanks.class, "tileEntityPlanks");
 		GameRegistry.registerTileEntity(TileEntityEdgeStone.class, "tileEntityedgestone");
 		GameRegistry.registerTileEntity(TileEntityEdgeStoneCorner.class, "tileEntityedgecorner");
 		GameRegistry.registerTileEntity(TileEntityGoblet.class, "tileEntitygoblet");
