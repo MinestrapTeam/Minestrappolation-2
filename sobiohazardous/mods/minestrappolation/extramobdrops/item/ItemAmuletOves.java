2c739f962c1ae3728959fec28d227ddf7751732b
Clashsoft <clash2@hotmail.de>
2014-05-09 00:04:52 +0200

    Re-added ItemAmuletOves

---
+++ sobiohazardous/mods/minestrappolation/extramobdrops/item/ItemAmuletOves.java
@@ -0,0 +1,12 @@
+package sobiohazardous.mods.minestrappolation.extramobdrops.item;
+
+
+public class ItemAmuletOves extends ItemAmulet
+{
+	public ItemAmuletOves()
+	{
+		super(0);
+	}
+	
+	// reduce damage to player occaisonally.
+}
