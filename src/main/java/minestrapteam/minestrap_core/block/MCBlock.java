7e91ae01b841b184d5052408efbe429ec90e7714
Domenic Portuesi <sobiohazardous@gmail.com>
2014-07-02 16:24:19 -0400

    Fixed block drops

--- src/main/java/minestrapteam/minestrap_core/block/MCBlock.java
+++ src/main/java/minestrapteam/minestrap_core/block/MCBlock.java
@@ -1,7 +1,12 @@
 package minestrapteam.minestrap_core.block;
 
+import java.util.Random;
+
+import minestrapteam.minestrappolation.lib.MBlocks;
+import minestrapteam.minestrappolation.lib.MItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 
 public class MCBlock extends Block
 {
@@ -9,4 +14,33 @@ public class MCBlock extends Block
 	{
 		super(material);
 	}
+	
+    public int quantityDropped(Random p_149745_1_)
+    {
+    	return 1;
+    }
+    
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+    {
+    	if(this == MBlocks.meuroditeOre)
+    	{
+    		return MItems.meuroditeGem;
+    	}
+    	if(this == MBlocks.radiantQuartzOre)
+    	{
+    		return MItems.radiantQuartz;
+    	}
+    	
+    	return Item.getItemFromBlock(this);
+    }
+    
+    protected boolean canSilkHarvest()
+    {
+    	if(this == MBlocks.meuroditeOre || this == MBlocks.radiantQuartzOre)
+    	{
+    		return true;
+    	}
+    	return super.canSilkHarvest();
+    }
+
 }
