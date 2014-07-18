582c758c24cd8c274cc6c99c188cc0b4c23470c6
Clashsoft <clashsoft@hotmail.com>
2014-07-18 21:53:23 +0200

    Ropes and Rope Coils can be mined with shears now

--- src/main/java/minestrapteam/minestrappolation/block/BlockRopeCoil.java
+++ src/main/java/minestrapteam/minestrappolation/block/BlockRopeCoil.java
@@ -1,15 +1,19 @@
 package minestrapteam.minestrappolation.block;
 
+import java.util.ArrayList;
+
 import minestrapteam.mcore.util.MCAssetManager;
 
 import net.minecraft.block.BlockFalling;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.IShearable;
 
-public class BlockRopeCoil extends BlockFalling
+public class BlockRopeCoil extends BlockFalling implements IShearable
 {
 	private IIcon	top;
 	
@@ -41,5 +45,19 @@ public class BlockRopeCoil extends BlockFalling
 	public boolean isLadder(IBlockAccess world, int x, int y, int z, EntityLivingBase entity)
 	{
 		return true;
+	}
+
+	@Override
+	public boolean isShearable(ItemStack stack, IBlockAccess world, int x, int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public ArrayList<ItemStack> onSheared(ItemStack stack, IBlockAccess world, int x, int y, int z, int metadata)
+	{
+		ArrayList<ItemStack> list = new ArrayList();
+		list.add(new ItemStack(this.getItemDropped(metadata, null, 0), 1, 0));
+		return list;
 	}
 }
