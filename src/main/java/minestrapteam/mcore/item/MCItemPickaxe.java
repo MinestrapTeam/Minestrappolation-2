63fa4839c134079a8cf6f8ebe67dca0a4a44db84
Clashsoft <clashsoft@hotmail.com>
2014-07-20 23:47:25 +0200

    Fixed Pickaxe issues
    Fixed Minestrappolation Pickaxes not being able to harvest certain
    blocks.
    Closes #193

--- src/main/java/minestrapteam/mcore/item/MCItemPickaxe.java
+++ src/main/java/minestrapteam/mcore/item/MCItemPickaxe.java
@@ -4,6 +4,7 @@ import clashsoft.cslib.minecraft.item.datatools.ItemDataPickaxe;
 import minestrapteam.minestrappolation.Minestrappolation;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
@@ -28,9 +29,37 @@ public class MCItemPickaxe extends MCItemTool
 	{
 		if (this.breakBedrock && block == Blocks.bedrock)
 		{
-			return this.toolMaterial.getHarvestLevel() == 4;
+			return this.toolMaterial.getHarvestLevel() >= 4;
 		}
-		return super.canHarvestBlock(block, stack);
+		if (block == Blocks.obsidian)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 3;
+		}
+		if (block == Blocks.diamond_block || block == Blocks.diamond_ore)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 2;
+		}
+		if (block == Blocks.emerald_ore || block == Blocks.emerald_block)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 2;
+		}
+		if (block == Blocks.gold_block || block == Blocks.gold_ore)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 2;
+		}
+		if (block == Blocks.iron_block || block == Blocks.iron_ore)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 1;
+		}
+		if (block == Blocks.lapis_block || block == Blocks.lapis_ore)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 1;
+		}
+		if (block == Blocks.redstone_ore || block == Blocks.lit_redstone_ore)
+		{
+			return this.toolMaterial.getHarvestLevel() >= 2;
+		}
+		return block.getMaterial() == Material.rock || block.getMaterial() == Material.iron || block.getMaterial() == Material.anvil;
 	}
 	
 	@Override
