6f11e87b39d41055e85b0c3908f3faf1ffab1590
Domenic Portuesi <sobiohazardous@gmail.com>
2014-07-22 01:32:17 -0400

    1.7.2-3.0.3

--- src/main/java/minestrapteam/mcore/lib/MCReference.java
+++ src/main/java/minestrapteam/mcore/lib/MCReference.java
@@ -10,13 +10,13 @@ public class MCReference
 	public static final String			CORE_MODID		= "mcore";
 	public static final String			CORE_ACRONYM	= "MC";
 	public static final String			CORE_NAME		= "Minestrappolation Core";
-	public static final String			CORE_VERSION	= "3.0.2";
+	public static final String			CORE_VERSION	= "3.0.3";
 	public static final String			CORE_DEPENDENCY	= "required-after:" + BrewingAPI.MODID;
 	
 	public static final String			MODID			= "minestrappolation";
 	public static final String			ACRONYM			= "M";
 	public static final String			NAME			= "Minestrappolation";
-	public static final String			VERSION			= "3.0.2";
+	public static final String			VERSION			= "3.0.3";
 	public static final String			DEPENDENCY		= "required-after:" + CORE_MODID;
 	
 	public static final List<String>	AUTHORS			= Arrays.asList("SoBiohazardous", "Clashsoft", "Delocuro", "Thewerty1124", "Triplg3");
