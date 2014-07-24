63e7c366248ff547c864e8eb94dbcacf095084fc
Clashsoft <clashsoft@hotmail.com>
2014-07-24 21:59:06 +0200

    Update MReference

--- src/main/java/minestrapteam/minestrappolation/lib/MReference.java
+++ src/main/java/minestrapteam/minestrappolation/lib/MReference.java
@@ -7,17 +7,11 @@ import clashsoft.brewingapi.BrewingAPI;
 
 public class MReference
 {
-	public static final String			CORE_MODID		= "mcore";
-	public static final String			CORE_ACRONYM	= "MC";
-	public static final String			CORE_NAME		= "Minestrappolation Core";
-	public static final String			CORE_VERSION	= "3.1.1";
-	public static final String			CORE_DEPENDENCY	= "required-after:" + BrewingAPI.MODID;
+	public static final String			MODID		= "minestrappolation";
+	public static final String			ACRONYM		= "M";
+	public static final String			NAME		= "Minestrappolation";
+	public static final String			VERSION		= "3.1.1";
+	public static final String			DEPENDENCY	= "required-after:" + BrewingAPI.MODID;
 	
-	public static final String			MODID			= "minestrappolation";
-	public static final String			ACRONYM			= "M";
-	public static final String			NAME			= "Minestrappolation";
-	public static final String			VERSION			= "3.1.1";
-	public static final String			DEPENDENCY		= "required-after:" + CORE_MODID;
-	
-	public static final List<String>	AUTHORS			= Arrays.asList("SoBiohazardous", "Clashsoft", "Delocuro", "Thewerty1124", "Triplg3");
+	public static final List<String>	AUTHORS		= Arrays.asList("SoBiohazardous", "Clashsoft", "Delocuro", "Thewerty1124");
 }
