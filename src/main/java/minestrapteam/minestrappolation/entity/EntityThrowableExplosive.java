6618fc4640651c24a56420434b9edbcb9ae1cb56
Clashsoft <clashsoft@hotmail.com>
2014-07-30 15:00:20 +0200

    Improved Grenade physics

--- src/main/java/minestrapteam/minestrappolation/entity/EntityThrowableExplosive.java
+++ src/main/java/minestrapteam/minestrappolation/entity/EntityThrowableExplosive.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public abstract class EntityThrowableExplosive extends EntityThrowable
@@ -32,12 +33,19 @@ public abstract class EntityThrowableExplosive extends EntityThrowable
 	@Override
 	public void onUpdate()
 	{
-		this.worldObj.spawnParticle("smoke", this.posX, this.posY + 0.5D, this.posZ, 0.0D, 0.0F, 0.0D);
-		
 		double prevVelX = this.motionX;
 		double prevVelY = this.motionY;
 		double prevVelZ = this.motionZ;
 		this.moveEntity(prevVelX, prevVelY, prevVelZ);
+		
+		Vec3 vec3 = Vec3.createVectorHelper(this.posX, this.posY, this.posZ);
+		Vec3 vec31 = Vec3.createVectorHelper(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
+		MovingObjectPosition movingobjectposition = this.worldObj.rayTraceBlocks(vec3, vec31);
+		
+		if (movingobjectposition != null)
+		{
+			this.onImpact(movingobjectposition);
+		}
 		
 		if (this.motionX != prevVelX)
 		{
@@ -51,12 +59,22 @@ public abstract class EntityThrowableExplosive extends EntityThrowable
 		{
 			this.motionZ = -this.bounceFactor * prevVelZ;
 		}
-		this.motionY -= 0.04; // Apply gravity.
+		
+		// Apply gravity
+		this.motionY -= 0.04;
 		
 		// Air friction
 		this.motionX *= 0.9;
 		this.motionY *= 0.9;
 		this.motionZ *= 0.9;
+		
+		this.updateFuse();
+	}
+	
+	protected final void updateFuse()
+	{
+		// Spawn smoke particles
+		this.worldObj.spawnParticle("smoke", this.posX, this.posY + 0.5D, this.posZ, 0.0D, 0.0F, 0.0D);
 		
 		// Are we going to explode?
 		if (this.fuse-- <= 0 && !this.worldObj.isRemote)
