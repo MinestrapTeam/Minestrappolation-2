b2849415fcda2099cd8542f7eed5598f96847b91
Clashsoft <clashsoft@hotmail.com>
2014-08-01 01:49:35 +0200

    Fixed Sawmill using Workbench recipes

--- src/main/java/minestrapteam/minestrappolation/inventory/ContainerSawmill.java
+++ src/main/java/minestrapteam/minestrappolation/inventory/ContainerSawmill.java
@@ -1,18 +1,23 @@
 package minestrapteam.minestrappolation.inventory;
 
+import minestrapteam.minestrappolation.crafting.sawmill.SawingManager;
 import minestrapteam.minestrappolation.tileentity.TileEntitySawmill;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ContainerWorkbench;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.world.World;
 
 public class ContainerSawmill extends ContainerWorkbench
 {
+	public World worldObj;
 	public TileEntitySawmill	sawmill;
 	
 	public ContainerSawmill(InventoryPlayer inventory, TileEntitySawmill sawmill)
 	{
 		super(inventory, sawmill.getWorldObj(), sawmill.xCoord, sawmill.yCoord, sawmill.zCoord);
+		this.worldObj = sawmill.getWorldObj();
 		this.sawmill = sawmill;
 	}
 	
@@ -21,4 +26,10 @@ public class ContainerSawmill extends ContainerWorkbench
 	{
 		return this.sawmill.isUseableByPlayer(player);
 	}
+	
+	@Override
+	public void onCraftMatrixChanged(IInventory p_75130_1_)
+    {
+        this.craftResult.setInventorySlotContents(0, SawingManager.instance.findMatchingRecipe(this.craftMatrix, this.worldObj));
+    }
 }
