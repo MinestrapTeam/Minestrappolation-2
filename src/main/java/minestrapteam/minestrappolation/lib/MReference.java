7263e9d35f45484db14c5c7f19deb202bd82e0a3
Clashsoft <clashsoft@hotmail.com>
2014-08-04 00:41:16 +0200

    1.7.10-3.2.0
    1000 Commits!

--- src/main/java/minestrapteam/minestrappolation/lib/MReference.java
+++ src/main/java/minestrapteam/minestrappolation/lib/MReference.java
@@ -10,7 +10,7 @@ public class MReference
 	public static final String			MODID		= "minestrappolation";
 	public static final String			ACRONYM		= "M";
 	public static final String			NAME		= "Minestrappolation";
-	public static final String			VERSION		= "3.1.1";
+	public static final String			VERSION		= "3.2.0";
 	public static final String			DEPENDENCY	= "required-after:" + BrewingAPI.MODID;
 	
 	public static final List<String>	AUTHORS		= Arrays.asList("SoBiohazardous", "Clashsoft", "Delocuro", "Thewerty1124");
