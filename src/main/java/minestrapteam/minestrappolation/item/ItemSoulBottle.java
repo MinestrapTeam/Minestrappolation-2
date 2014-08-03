4e85b052861ab947e7236385dec05151dcea756a
Clashsoft <clashsoft@hotmail.com>
2014-08-03 14:22:48 +0200

    Removed Soul Bottle WIP label

--- src/main/java/minestrapteam/minestrappolation/item/ItemSoulBottle.java
+++ src/main/java/minestrapteam/minestrappolation/item/ItemSoulBottle.java
@@ -1,12 +1,9 @@
 package minestrapteam.minestrappolation.item;
 
-import java.util.List;
-
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
 public class ItemSoulBottle extends Item
@@ -33,14 +30,6 @@ public class ItemSoulBottle extends Item
 				player.dropItem(Items.experience_bottle, 1);
 			}
 		}
-		return stack;
-		
+		return stack;	
 	}
-	
-	@Override
-	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean flag)
-	{
-		list.add(EnumChatFormatting.RED + "WIP");
-	}
-	
 }
