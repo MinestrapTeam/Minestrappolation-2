6a217d2688279e4ddab3c71edeededde0381590d
Clashsoft <clashsoft@hotmail.com>
2014-08-04 20:34:55 +0200

    Fixed Redwood Sapling issues
    Closes #240

--- src/main/java/minestrapteam/minestrappolation/block/BlockRedwoodSapling.java
+++ src/main/java/minestrapteam/minestrappolation/block/BlockRedwoodSapling.java
@@ -2,13 +2,14 @@ package minestrapteam.minestrappolation.block;
 
 import minestrapteam.minestrappolation.world.gen.WorldGenBaseTree;
 import minestrapteam.minestrappolation.world.gen.WorldGenRedWoodTree;
+import minestrapteam.minestrappolation.world.gen.WorldGenRedWoodTreeSmall;
 
 public class BlockRedwoodSapling extends MBlockSapling
 {
 	@Override
-	public WorldGenBaseTree getTreeToGrow()
+	public WorldGenBaseTree getTreeToGrow(int metadata, boolean big)
 	{
-		return new WorldGenRedWoodTree();
+		return big ? new WorldGenRedWoodTree() : new WorldGenRedWoodTreeSmall();
 	}
 	
 }
--- src/main/java/minestrapteam/minestrappolation/block/MBlockSapling.java
+++ src/main/java/minestrapteam/minestrappolation/block/MBlockSapling.java
@@ -7,8 +7,10 @@ import minestrapteam.minestrappolation.world.gen.WorldGenBaseTree;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.IGrowable;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.event.terraingen.TerrainGen;
 
 public abstract class MBlockSapling extends BlockBush implements IGrowable
@@ -16,6 +18,12 @@ public abstract class MBlockSapling extends BlockBush implements IGrowable
 	public MBlockSapling()
 	{
 		this.setBlockBounds(0.1F, 0.0F, 0.1F, 0.9F, 0.8F, 0.9F);
+	}
+	
+	@Override
+	public int getFlammability(IBlockAccess world, int x, int y, int z, ForgeDirection face)
+	{
+		return 255;
 	}
 	
 	@Override
@@ -32,7 +40,19 @@ public abstract class MBlockSapling extends BlockBush implements IGrowable
 		}
 	}
 	
-	public abstract WorldGenBaseTree getTreeToGrow();
+	public void updateState(World world, int x, int y, int z, Random random)
+	{
+		int l = world.getBlockMetadata(x, y, z);
+		
+		if ((l & 8) == 0)
+		{
+			world.setBlockMetadataWithNotify(x, y, z, l | 8, 4);
+		}
+		else
+		{
+			this.growTree(world, x, y, z, random);
+		}
+	}
 	
 	public void growTree(World world, int x, int y, int z, Random rand)
 	{
@@ -41,14 +61,59 @@ public abstract class MBlockSapling extends BlockBush implements IGrowable
 			return;
 		}
 		
-		int l = world.getBlockMetadata(x, y, z) & 7;
-		WorldGenerator worldGen = this.getTreeToGrow();
+		int metadata = world.getBlockMetadata(x, y, z) & 7;
+		boolean flag = false;
+		int i1 = 0;
+		int j1 = 0;
 		
-		world.setBlock(x, y, z, Blocks.air, 0, 4);
-		if (!worldGen.generate(world, rand, x + 0, y, z + 0))
+		outer:
+		for (i1 = 0; i1 >= -1; --i1)
 		{
-			world.setBlock(x, y, z, this, l, 4);
+			for (j1 = 0; j1 >= -1; --j1)
+			{
+				if (this.isBlock(world, x + i1, y, z + j1, metadata) && this.isBlock(world, x + i1 + 1, y, z + j1, metadata) && this.isBlock(world, x + i1, y, z + j1 + 1, metadata) && this.isBlock(world, x + i1 + 1, y, z + j1 + 1, metadata))
+				{
+					flag = true;
+					break outer;
+				}
+			}
 		}
+		
+		WorldGenerator worldGen = this.getTreeToGrow(metadata & 7, flag);
+		
+		if (flag)
+        {
+            world.setBlock(x + i1, y, z + j1, Blocks.air, 0, 4);
+            world.setBlock(x + i1 + 1, y, z + j1, Blocks.air, 0, 4);
+            world.setBlock(x + i1, y, z + j1 + 1, Blocks.air, 0, 4);
+            world.setBlock(x + i1 + 1, y, z + j1 + 1, Blocks.air, 0, 4);
+        }
+        else
+        {
+            world.setBlock(x, y, z, Blocks.air, 0, 4);
+        }
+
+        if (!worldGen.generate(world, rand, x + i1, y, z + j1))
+        {
+            if (flag)
+            {
+                world.setBlock(x + i1, y, z + j1, this, metadata, 4);
+                world.setBlock(x + i1 + 1, y, z + j1, this, metadata, 4);
+                world.setBlock(x + i1, y, z + j1 + 1, this, metadata, 4);
+                world.setBlock(x + i1 + 1, y, z + j1 + 1, this, metadata, 4);
+            }
+            else
+            {
+                world.setBlock(x, y, z, this, metadata, 4);
+            }
+        }
+	}
+	
+	public abstract WorldGenBaseTree getTreeToGrow(int metadata, boolean big);
+	
+	private boolean isBlock(World world, int x, int y, int z, int metadata)
+	{
+		return world.getBlock(x, y, z) == this && (world.getBlockMetadata(x, y, z) & 7) == metadata;
 	}
 	
 	@Override
@@ -66,6 +131,6 @@ public abstract class MBlockSapling extends BlockBush implements IGrowable
 	@Override
 	public void func_149853_b(World world, Random random, int x, int y, int z)
 	{
-		this.growTree(world, x, y, z, random);
+		this.updateState(world, x, y, z, random);
 	}
 }
