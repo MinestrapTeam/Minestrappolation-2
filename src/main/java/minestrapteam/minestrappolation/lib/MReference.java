ea6ffdcca0b5afe2c893a84f1dd0218d042e8408
Domenic Portuesi <sobiohazardous@gmail.com>
2014-09-21 18:14:00 -0400

    1.7.10-3.3.1.1

--- src/main/java/minestrapteam/minestrappolation/lib/MReference.java
+++ src/main/java/minestrapteam/minestrappolation/lib/MReference.java
@@ -10,7 +10,7 @@ public class MReference
 	public static final String			MODID		= "minestrappolation";
 	public static final String			ACRONYM		= "M";
 	public static final String			NAME		= "Minestrappolation";
-	public static final String			VERSION		= "1.7.10-3.3.1";
+	public static final String			VERSION		= "1.7.10-3.3.1.1";
 	public static final String			DEPENDENCY	= "required-after:" + BrewingAPI.MODID;
 	
 	public static final List<String>	AUTHORS		= Arrays.asList("SoBiohazardous", "Clashsoft", "Delocuro", "Thewerty1124", "LandKingdom");
