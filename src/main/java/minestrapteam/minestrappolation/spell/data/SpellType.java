309afc811493ff8282323be537c47d63048b77f2
Clashsoft <clashsoft@hotmail.com>
2014-10-14 16:53:04 +0200

    Improved Spell Type colors

--- src/main/java/minestrapteam/minestrappolation/spell/data/SpellType.java
+++ src/main/java/minestrapteam/minestrappolation/spell/data/SpellType.java
@@ -10,14 +10,14 @@ public class SpellType
 {
 	public static SpellType[]		spellTypes	= new SpellType[8];
 	
-	public static final SpellType	WATER		= new SpellType(0, "water", 0x347A9D, EnumChatFormatting.DARK_AQUA);
-	public static final SpellType	FIRE		= new SpellType(1, "fire", 0x9D6834, EnumChatFormatting.GOLD);
-	public static final SpellType	EARTH		= new SpellType(2, "earth", 0x699D34, EnumChatFormatting.GREEN);
-	public static final SpellType	WIND		= new SpellType(3, "wind", 0x819898, EnumChatFormatting.GRAY);
-	public static final SpellType	FROST		= new SpellType(4, "frost", 0x69B6B6, EnumChatFormatting.AQUA);
-	public static final SpellType	ELECTRICITY	= new SpellType(5, "electricity", 0xDCB643, EnumChatFormatting.YELLOW);
-	public static final SpellType	LIFE		= new SpellType(6, "life", 0xC13030, EnumChatFormatting.RED);
-	public static final SpellType	ARCANE		= new SpellType(7, "arcane", 0x4848C2, EnumChatFormatting.DARK_PURPLE);
+	public static final SpellType	WATER		= new SpellType(0, "water", 0x608FEE, EnumChatFormatting.DARK_AQUA);
+	public static final SpellType	FIRE		= new SpellType(1, "fire", 0xFEAF11, EnumChatFormatting.GOLD);
+	public static final SpellType	EARTH		= new SpellType(2, "earth", 0x72A73C, EnumChatFormatting.GREEN);
+	public static final SpellType	WIND		= new SpellType(3, "wind", 0x828F8F, EnumChatFormatting.GRAY);
+	public static final SpellType	FROST		= new SpellType(4, "frost", 0x4FA4A4, EnumChatFormatting.AQUA);
+	public static final SpellType	ELECTRICITY	= new SpellType(5, "electricity", 0xE1E134, EnumChatFormatting.YELLOW);
+	public static final SpellType	LIFE		= new SpellType(6, "life", 0xEF6060, EnumChatFormatting.RED);
+	public static final SpellType	ARCANE		= new SpellType(7, "arcane", 0xB765E1, EnumChatFormatting.DARK_PURPLE);
 	
 	public int						id;
 	public String					name;
